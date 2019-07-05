665622152c8e11f3829a726f3174ba36cb97c86c
Weiguang <chen.weiguang@gmail.com>
2019-07-05 08:06:54 +0800

    Fix typo in env var (#3)

--- push-images.sh
+++ push-images.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 DOCKER_IMAGE=${DOCKER_IMAGE:-guangie88/spark-k8s-addons}
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_IMAGE}:${SPARK_VERSION}_hadoop-${HADOOP}"
+docker push "${DOCKER_IMAGE}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
