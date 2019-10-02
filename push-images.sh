fb0e0ba9376ec9fd16d02e5779d705608c0c2218
Chen Weiguang <chen.weiguang@gmail.com>
2019-10-02 16:44:17 +0800

    Update to build for the latest master

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-guangie88/spark-k8s-addons}
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_IMAGE}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+
+TAG_NAME="${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+
+docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
