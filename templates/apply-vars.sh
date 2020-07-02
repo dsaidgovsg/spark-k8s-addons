ed6e0dbe22f7404485038e986a2d9b49a08e6d1d
Weiguang <chen.weiguang@gmail.com>
2020-07-02 16:26:07 +0800

    feat(matrix): support Spark 2.4.6 and 3.0.0 (#38)

--- templates/apply-vars.sh
+++ templates/apply-vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ ! -v TERA ]] && ! command -v tera >/dev/null; then
-    printf "Run \"cargo install tera-cli\" to install tera first.\nLinux users may download from https://github.com/guangie88/tera-cli/releases instead."
+    printf "Run \"cargo install tera-cli --version=^0.4.0\" to install tera first.\nLinux users may download from https://github.com/guangie88/tera-cli/releases instead."
     return 1
 elif [[ ! -v TERA ]]; then
     TERA="$(command -v tera)"
